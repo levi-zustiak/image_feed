62f234b24a97c76129624fa99e3c920583bf2550
Levi Zustiak <levizustiak@Pocket-Sand.local>
2021-02-08 11:09:33 -0600

    Added Card Component

--- App.js
+++ App.js
@@ -3,19 +3,19 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Constants from 'expo-constants';
 
-import Avatar from './components/Avatar';
-import AuthorRow from './components/AuthorRow';
+import Card from './components/Card';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <AuthorRow
+        <Card
           fullname={'First Last'}
           linkText={'Comments'}
           onPressLinkText={() => {
             console.log('Pressed link!');
           }}
+          image={{ uri: 'https://unsplash.it/600/600' }}
         />
       </View>
     );
